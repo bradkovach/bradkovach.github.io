fbe83323709bf56ac4acb7b40a2f6498abbe1735
Brad Kovach <bradkovach@gmail.com>
2024-04-10 08:19:57 -0600

    Consolidate pricing driver api

--- scripts/choice-gas/com.choicegas.ts
+++ scripts/choice-gas/com.choicegas.ts
@@ -1,22 +1,15 @@
 import cheerio from 'cheerio';
-import fs from 'node:fs';
 import { Market } from '../../projects/bradkovach.github.io/src/app/routes/choice-gas/data/data.current';
 import {
 	MarketOffer,
-	Offer,
 	OfferBase,
 } from '../../projects/bradkovach.github.io/src/app/routes/choice-gas/entity/Offer';
 
-type ExtractFn<T extends Offer> = (
-	term: number,
-	name: string,
-	priceText: string,
-	confirmationCode: string,
-) => T;
-function run() {
-	const url =
-		'https://www.blackhillsenergy.com/services/choice-gas-program/wyoming-choice-gas-customers/black-hills-wyoming-gas-llc-utility-gas';
-	fetch(url)
+const url =
+	'https://www.blackhillsenergy.com/services/choice-gas-program/wyoming-choice-gas-customers/black-hills-wyoming-gas-llc-utility-gas';
+
+export function run(): Promise<(OfferBase & MarketOffer)[]> {
+	return fetch(url)
 		.then((response) => response.text())
 		.then((text) => {
 			const $ = cheerio.load(text);
@@ -24,35 +17,30 @@ function run() {
 		})
 		.then(($) =>
 			$(
-				'.article--content__description > table:nth-of-type(1) > tbody:nth-of-type(1) > tr',
-			).map((rowIdx, tr) => {
-				const tds = $(tr).find('td:not(colspan)');
-				const term = Math.floor(rowIdx / 4) + 1;
-				const [division, priceText, confirmationCode] = tds
-					.toArray()
-					.slice(-3)
-					.map((td) => $(td).text().trim());
+				'.article--content__description > table:nth-of-type(1) > tbody:nth-of-type(1) > tr:nth-of-type(1)',
+			)
+				.map((rowIdx, tr): OfferBase & MarketOffer => {
+					const tds = $(tr).find('td:not(colspan)');
+					const term = Math.floor(rowIdx / 4) + 1;
+					const [division, priceText, confirmationCode] = tds
+						.toArray()
+						.slice(-3)
+						.map((td) => $(td).text().trim());
 
-				const offer: OfferBase & MarketOffer = {
-					type: 'market',
-					id: `gca-${division}-${term}`,
-					name: 'Gas Cost Adjustment - ' + division + ' Division',
-					term,
-					market: Market.GCA,
-					rate: 0,
-					confirmationCode,
-				};
-
-				return offer;
-			}),
-		)
-		.then((offers) => {
-			// write to json file at path
-			const path =
-				'projects/bradkovach.github.io/src/app/routes/choice-gas/data/vendors/json/com.choicegas.json';
-			const json = JSON.stringify(offers.toArray(), null, 2);
-			fs.writeFileSync(path, json, 'utf-8');
-		});
+					return {
+						type: 'market',
+						id: `gca-${division}-${term}`,
+						name: `Gas Cost Adjustment - $${Number(
+							priceText,
+						).toFixed(4)} - ${division} Division`,
+						term,
+						market: Market.GCA,
+						rate: 0,
+						confirmationCode,
+					};
+				})
+				.toArray(),
+		);
 }
 
-run();
+// run();
